b70be5a293a949dd9666cd7b6f5e23edd9f4101b
Bogdan Sass <bogd.no.spam@gmail.com>
2019-02-19 00:20:38 +0200

    Added background color

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
-FROM frolvlad/alpine-python3
+FROM alpine
+
+RUN apk add python3
+
+ENV COLOR="#0000FF"
+
 COPY test.py /
+
 COPY get_details.sh /
+
+EXPOSE 8081/tcp
+
 CMD ["/test.py"]
--- get_details.sh
+++ get_details.sh
@@ -1,7 +1,12 @@
-echo '<font color="red">IP Addresses:</font></p>'
+echo "<html><head><style> body {"
+echo "background-color: $COLOR;"
+echo "} p {background-color: #FFFFFF;}"
+echo "</style></head><body>"
+
+echo '<p><font color="red">IP Addresses:</font></p><p>'
 ifconfig | egrep -o "^[a-z0-9]+|addr:[0-9\.]+" | sed "s/$/<p>/"
 echo "<p>"
 echo '<font color="red">Hostname</font><p>'
 cat /etc/hostname
 echo "<p>"
-
+echo "</body></html>"
